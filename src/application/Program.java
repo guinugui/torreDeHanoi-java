bdb6d35fee0edf5d1f639cd58fd1ebe5ddff7543
Guilherme Ribeiro <guilhermevendaseprodutos@gmail.com>
2024-02-29 23:10:54 -0300

    Programa principal concluido

--- src/application/Program.java
+++ src/application/Program.java
@@ -8,9 +8,12 @@ public class Program {
 	public static void main(String[] args) {
 		Locale.setDefault(Locale.US);
 		Scanner sc = new Scanner(System.in);
-		
-		
-		
+
+		System.out.println("Digite o numero de discos na Torre: ");
+		int n = sc.nextInt();
+
+		movimentoTorre.movimento(n, 'A', 'B', 'C');
+
 		sc.close();
 
 	}
--- src/application/movimentoTorre.java
+++ src/application/movimentoTorre.java
@@ -6,38 +6,45 @@ public class movimentoTorre {
 	private String aux;
 	private String fim;
 	private Integer n;
+
 	public String getComeco() {
 		return comeco;
 	}
+
 	public void setComeco(String comeco) {
 		this.comeco = comeco;
 	}
+
 	public String getAux() {
 		return aux;
 	}
+
 	public void setAux(String aux) {
 		this.aux = aux;
 	}
+
 	public String getFim() {
 		return fim;
 	}
+
 	public void setFim(String fim) {
 		this.fim = fim;
 	}
+
 	public Integer getN() {
 		return n;
 	}
+
 	public void setN(Integer n) {
 		this.n = n;
 	}
-	public static void movimento(String comeco, String aux, String fim, int n) {
-		if(n >= 1) {
-			movimento(comeco, aux, fim, n - 1);
-			System.out.println("O disco " + n + " se moveu para torre " + comeco + "para a torre " + fim + " !");
-			movimento(comeco, aux, fim, n - 1);
+
+	public static void movimento(int n, char comeco, char aux, char fim) {
+		if (n > 0) {
+			movimento(n - 1, comeco, fim, aux);
+			System.out.println("O disco " + n + " se moveu da torre " + comeco + " para a torre " + fim + " !");
+			movimento(n - 1, aux, comeco, fim);
 		}
 	}
-	
-	
-	
+
 }
